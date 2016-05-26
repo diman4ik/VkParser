af886e9801b7684e431b921c08abe53c9df3f6f4
diman4ik <diman4ik.chernov@gmail.com>
2016-05-26 11:38:42 +0300

    progressbar

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def refreshClicked():
         city = ui.cityEdit.text()
         age_from = ui.spinBoxAgeFrom.text()
         age_to = ui.spinBoxAgeTo.text()
-        QtGui.QMessageBox.about(QtGui.QWidget(), "Супер прога", city)
+        #QtGui.QMessageBox.about(QtGui.QWidget(), "Супер прога", city)
         return city, age_from, age_to
         
     return None
@@ -47,39 +47,46 @@ def enableAllControls(enable):
     return False
     
 def createProgress(dialog):
+    global progress
     progress = QtGui.QProgressBar(dialog)
     progress.setGeometry(200, 80, 250, 20)
     progress.move(450, 400)
-    progress.setValue(50)
     
 def getUsersFromCity(api, pcity):
     # 365 запросов по всем дням в году - так преодолемм ограничение не больше 1000    
     request_count = 0 # не более 3 в секунду, делаем два и ждем пару сек
     
+    total_requests = 27
+    done_requests = 0
+    
     for m in range(1, 4): # месяцы
         for d in range(1, 4): #дни
             tot_count=3
-            result = api.users.search(city=pcity, birth_day=d, birth_month=m, count=tot_count, fields='sex, bdate')
+            #result = api.users.search(city=pcity, birth_day=d, birth_month=m, count=tot_count, fields='sex, bdate')
             
             #result = json.loads(result)
-            print(result)
+            #print(result)
             
-            for ind in range(1, len(result)):
-                age = 0
+            #for ind in range(1, len(result)):
+            #    age = 0
             
-                if 'bdate' in result[ind]:
-                    strage = result[ind]['bdate'].split('.')[-1]
+            #    if 'bdate' in result[ind]:
+            #        strage = result[ind]['bdate'].split('.')[-1]
                     
-                    if len(strage) == 4:
-                        age = 2016 - int(strage)
+            #        if len(strage) == 4:
+            #            age = 2016 - int(strage)
             
-                songdb.addUser(result[ind]['uid'], result[ind]['first_name'], result[ind]['last_name'], result[ind]['sex'], pcity, age)
+                #songdb.addUser(result[ind]['uid'], result[ind]['first_name'], result[ind]['last_name'], result[ind]['sex'], pcity, age)
         
             if request_count == 2:
                 time.sleep(2)
                 request_count = 0
+                
+                global progress
+                progress.setValue( int(done_requests/total_requests*100) )
             
             request_count += 1
+            done_requests += 1
     
     print("info added")
     
@@ -111,21 +118,22 @@ if __name__ == "__main__":
         #stoken, _ = auth_session.get_access_token()
         
         print(stoken)
-
+        
+        Dialog = QtGui.QDialog()
+        ui = main_form.Ui_Dialog()
+        ui.setupUi(Dialog)
+        
+        createProgress(Dialog)
+        Dialog.show()
+        
         session = vk.Session(access_token=stoken)
         vkapi = vk.API(session, lang='ru')
         
-        #getUsersFromCity(vkapi, 157)
+        getUsersFromCity(vkapi, 157)
         count = songdb.getUsersCount()
         print(count)
-    
-    Dialog = QtGui.QDialog()
-    ui = main_form.Ui_Dialog()
-    ui.setupUi(Dialog)
-    
-    createProgress(Dialog)
+        progress.hide()
     
     ui.peopleFilterButton.clicked.connect(refreshClicked)
 	
-    Dialog.show()
     sys.exit(app.exec_())
